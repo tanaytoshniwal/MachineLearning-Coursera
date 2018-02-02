3ae64b77694d1dd397a1d0abd251787bed2acc22
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2018-02-03 03:23:53 +0530

    using polynomial features

--- machine-learning-ex5/ex5/polyFeatures.m
+++ machine-learning-ex5/ex5/polyFeatures.m
@@ -16,9 +16,18 @@ X_poly = zeros(numel(X), p);
 % 
 
 
+m = size(X,1);
 
+for i=1:m
 
+    poly_feature = zeros(p, 1);
 
+    for j=1:p
+        poly_feature(j) =  X(i).^j;
+    end
+
+    X_poly(i, :) = poly_feature;
+end
 
 % =========================================================================
 
