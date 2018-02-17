fb3c6656429629d0355e75348747f3ef8fd5d187
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2018-02-17 19:29:59 +0530

    compute centroids

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -27,10 +27,13 @@ centroids = zeros(K, n);
 %
 
 
-
-
-
-
+for i = 1:K
+    c_i = idx==i;
+    n_i = sum(c_i);
+    c_i_matrix = repmat(c_i,1,n);
+    X_c_i = X .* c_i_matrix;
+    centroids(i,:) = sum(X_c_i) ./ n_i;
+end
 
 
 % =============================================================
